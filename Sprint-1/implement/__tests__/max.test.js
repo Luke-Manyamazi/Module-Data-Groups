62fc7d542bc2acecf8320593f7bd88f61720638a
luke-manyamazi <lukemanyamazi1@gmail.com>
2024-12-09 20:01:40 +0200

    added find max function and tests.

--- Sprint-1/implement/__tests__/max.test.js
+++ Sprint-1/implement/__tests__/max.test.js
@@ -16,28 +16,48 @@ const findMax = require("../max.js");
 // When passed to the max function
 // Then it should return -Infinity
 // Delete this test.todo and replace it with a test.
-test.todo("given an empty array, returns -Infinity");
+test("given an empty array, returns -Infinity", () => {
+  expect(findMax([]).toBe(-Infinity));
+});
 
 // Given an array with one number
 // When passed to the max function
 // Then it should return that number
+test("given an array with one number", () => {
+  expect(findMax[42]).toBe(42);
+});
 
 // Given an array with both positive and negative numbers
 // When passed to the max function
 // Then it should return the largest number overall
+test("given an array with both positive and negative numbers", () => {
+  expect(findMax([-4, -5, 9, 2])).toBe(9);
+});
 
 // Given an array with just negative numbers
 // When passed to the max function
 // Then it should return the closest one to zero
+test("given an array with jus negative numbers", () => {
+  expect(findMax([-8, -7, -2])).toBe(-2);
+});
 
 // Given an array with decimal numbers
 // When passed to the max function
 // Then it should return the largest decimal number
+test("given an array with decimal numbers", () => {
+  expect(findMax([2.9, 7.8, 9.4])).toBe(9.4);
+});
 
 // Given an array with non-number values
 // When passed to the max function
 // Then it should return the max and ignore non-numeric values
+test("given an array with non-number values", () => {
+  expect(findMax(["hi", "luke", "ball", 9, 5, 19])).toBe(19);
+});
 
 // Given an array with only non-number values
 // When passed to the max function
 // Then it should return the least surprising value given how it behaves for all other inputs
+test("given an array with only non-number values", () => {
+  expect(findMax(["luke", "hi", "tom", null])).toBe(-Infinity);
+});
--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,7 @@
 function findMax(elements) {
+  if (!Array.isArray(elements)) return -Infinity; // This handles non-array input
+  const numElements = elements.filter((item) => typeof item === "number"); // this checks if elements are numbers
+  return numElements.length > 0 ? Math.max(...numElements) : -Infinity; // math.max  returns the max number
 }
 
 module.exports = findMax;
