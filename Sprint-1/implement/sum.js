f1c7a0fc7417a1dc915e0239d28e17677a77d03b
luke-manyamazi <lukemanyamazi1@gmail.com>
2024-12-09 21:19:16 +0200

    fixed the sum function

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -2,7 +2,9 @@ function sum(elements) {
   const sum = 0;
 
   for (let i = 0; i < elements.length; i++) {
-    sum += sum + i;
+    if (typeof elements[i] === "number") {
+      sum += elements[i];
+    }
   }
   return sum;
 }
