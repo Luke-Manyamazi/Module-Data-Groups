ecde368c39ae734e3476a9f549d4d4aed289cf99
luke-manyamazi <lukemanyamazi1@gmail.com>
2024-12-09 21:15:02 +0200

    changed expected to expect in tests file

--- Sprint-1/implement/__tests__/sum.test.js
+++ Sprint-1/implement/__tests__/sum.test.js
@@ -14,42 +14,40 @@ const sum = require("../sum.js");
 // When passed to the sum function
 // Then it should return 0
 test("given an empty array, returns 0", () => {
-  expected(sum([])).toBe(0);
+  expect(sum([])).toBe(0);
 });
 
 // Given an array with just one number
 // When passed to the sum function
 // Then it should return that number
 test("given an array with just one number", () => {
-  expected(sum([20])).toBe(20);
+  expect(sum([20])).toBe(20);
 });
 
 // Given an array containing negative numbers
 // When passed to the sum function
 // Then it should still return the correct total sum
 test("given an array containing negative numbers", () => {
-  expected(sum([-3, -7, 0, 2])).toBe(-8);
+  expect(sum([-3, -7, 0, 2])).toBe(-8);
 });
 
 // Given an array with decimal/float numbers
 // When passed to the sum function
 // Then it should return the correct total sum
 test("given an array with decimal or float numbers", () => {
-  expected(sum([6.5, 2.1, 0.5])).toBe(9.1);
+  expect(sum([6.5, 2.1, 0.5])).toBe(9.1);
 });
 
 // Given an array containing non-number values
 // When passed to the sum function
 // Then it should ignore the non-numerical values and return the sum of the numerical elements
 test("given an array containing non-number values", () => {
-  expected(sum(["hi", "hello", 9, 2])).toBe(11);
+  expect(sum(["hi", "hello", 9, 2])).toBe(11);
 });
 
 // Given an array with only non-number values
 // When passed to the sum function
 // Then it should return the least surprising value given how it behaves for all other inputs
 test("given an array with only non-number values", () => {
-  expected(sum(["hi", null, undefined])).toBe(
-    "Please provide numerical values"
-  );
+  expect(sum(["hi", null, undefined])).toBe("Please provide numerical values");
 });
