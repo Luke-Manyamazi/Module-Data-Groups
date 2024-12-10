c987200bc918853afa6039f5110e275b93e8f7b1
luke-manyamazi <lukemanyamazi1@gmail.com>
2024-12-10 15:47:02 +0200

    added code to log each ingredient on a new line

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -2,7 +2,7 @@
 
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
-// How can you fix it?
+// How can you fix it? I will log each ingredient on a new line using the array indexing method
 
 const recipe = {
   title: "bruschetta",
@@ -10,6 +10,12 @@ const recipe = {
   ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
 };
 
-console.log(`${recipe.title} serves ${recipe.serves}
+console.log(
+  `${recipe.title} serves ${recipe.serves} 
   ingredients:
-${recipe}`);
+  ${recipe.ingredients[0]}
+  ${recipe.ingredients[1]}
+  ${recipe.ingredients[2]}
+  ${recipe.ingredients[3]}
+  `
+);
