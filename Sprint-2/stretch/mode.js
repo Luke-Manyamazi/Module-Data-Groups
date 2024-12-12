b8e1dacf7f1271177eea9467740873177caeaa2f
luke-manyamazi <lukemanyamazi1@gmail.com>
2024-12-12 18:57:46 +0200

    split the code into smaller functions

--- Sprint-2/stretch/mode.js
+++ Sprint-2/stretch/mode.js
@@ -8,21 +8,26 @@
 // refactor calculateMode by splitting up the code
 // into smaller functions using the stages above
 
-function calculateMode(list) {
-  // track frequency of each value
+
+// Function to filter only numbers from the input list
+function filterNumbers(list) {
+  return list.filter(item => typeof item === "number")
+}
+
+// Function to calculate frequencies of numbers
+function calculateFrequencies(numbers) {
   let freqs = new Map();
-
-  for (let num of list) {
-    if (typeof num !== "number") {
-      continue;
-    }
-
+  for (let num of numbers) {
     freqs.set(num, (freqs.get(num) || 0) + 1);
   }
+return freqs;
 
-  // Find the value with the highest frequency
+}
+
+// Function to find the mode from frequencies
+function findMode(freqs) {
   let maxFreq = 0;
-  let mode;
+  let mode = null;
   for (let [num, freq] of freqs) {
     if (freq > maxFreq) {
       mode = num;
@@ -33,4 +38,13 @@ function calculateMode(list) {
   return maxFreq === 0 ? NaN : mode;
 }
 
-module.exports = calculateMode;
+// Main function to calculate the mode
+function calculateMode(list) {
+  const numbers = filterNumbers(list);
+  const freqs = calculateFrequencies(numbers);
+  return findMode(freqs);
+}
+
+// module.exports = calculateMode;
+
+console.log(calculateMode([1, 2, 2, 3, 3, 3, 3, "a", {}, 2]));
