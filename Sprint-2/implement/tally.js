cb73da341da7a8f7fb04fa6fb982535d810db69f
luke-manyamazi <lukemanyamazi1@gmail.com>
2024-12-12 06:05:39 +0200

    changed throw new error to a return statement

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,14 @@
-function tally() {}
+function tally(items) {
+    if(!Array.isArray(items)) {
+        return "Invalid input";
+    }
+
+    const count = {};
+    for (const item of items) {
+        counts[item] = (counts[item] || 0) + 1;
+    }
+
+    return counts;
+}
 
 module.exports = tally;
