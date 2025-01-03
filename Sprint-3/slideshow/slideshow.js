10db52778f146e2cdae30b07e70d9c8ac1c621f5
Luke-Manyamazi <lukemanyamazi1@gmail.com>
2025-01-03 18:17:49 +0200

    added the new images to the js file

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -1,7 +1,9 @@
 const images = [
-    "./assets/cute-cat-a.png",
-    "./assets/cute-cat-b.jpg",
-    "./assets/cute-cat-c.jpg",
+    "./assets/codingDeskSetup1.jpg",
+    "./assets/codingDeskSetup2.jpg",
+    "./assets/codingDeskSetup3.jpg",
+    "./assets/codingDeskSetup4.jpg",
+    "./assets/codingDeskSetup5.jpg"   
 ];
 
 
