d861ba381502df80a572761b73e2ca5aef5e83f8
Luke-Manyamazi <lukemanyamazi1@gmail.com>
2025-01-07 13:40:02 +0200

    added new images, added css, added js, added html for the carousel

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -6,5 +6,42 @@ const images = [
     "./assets/codingDeskSetup5.jpg"   
 ];
 
+let slideIndex = 0;
+const imgElement = document.getElementById('carousel-img');    
+const dots = document.getElementsByClassName('dot'); 
+
+function showSlide(index) {
+    if (index >= images.length) {
+        slideIndex = 0;
+    } else if (index < 0) {
+        slideIndex = images.length - 1;
+    } else {
+        slideIndex = index;
+    }
+    imgElement.src = images[slideIndex];
+    updateDots()
+}
+
+function updateDots() {
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    dots[slideIndex].className += " active";
+}
+
+document.querySelector('.prev').addEventListener('click', () => {
+    showSlide(slideIndex - 1);
+});
+document.querySelector('.next').addEventListener('click', () => {
+    showSlide(slideIndex + 1);
+});
+
+for (let i = 0; i < dots.length; i++) {
+    dots[i].addEventListener('click', () => {
+        showSlide(i);
+    });
+}
+
+showSlide(slideIndex);
 
 // Write your code here
